d26b47cebf41221c71e2c58370c2e03e95b4b7fa
Ginta-Togawa <togawa.ginta.ag@gmail.com>
2023-01-21 18:50:11 +0900

    models.pyへのReportModelクラスの作成とDB環境作成

--- day_repo/models.py
+++ day_repo/models.py
@@ -1,3 +1,13 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+# 日報モデル
+class ReportModel(models.Model):
+    # ID(主キーは)自動生成のため割愛
+    # タイトル：100字以内
+    title = models.CharField('タイトル', max_length=100)
+    # 報告内容：1000字以内
+    content = models.CharField('報告内容', max_length=1000)
+    # 作成日時
+    created_date_time = models.DateTimeField('作成日時', auto_now_add=True)
