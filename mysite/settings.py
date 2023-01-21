b46807a9594efa68f96972f7418335ef1e2f060a
Ginta-Togawa <togawa.ginta.ag@gmail.com>
2023-01-21 18:32:51 +0900

    day_repoアプリを追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'day_repo'アプリをDjangoに追加
+    'day_repo',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 'day_repo/'を'day_repo'下の'urls.py'に紐づけ
+    path('day_repo/', include('day_repo.urls')),
 ]
