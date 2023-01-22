a1feb19edbe6004d687b553bc5d82232db5cf2b0
Ginta-Togawa <togawa.ginta.ag@gmail.com>
2023-01-23 02:40:44 +0900

    日報アプリの一覧画面作成

--- day_repo/urls.py
+++ day_repo/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from day_repo.views import day_repo_list_view
+
+# URLパターン(day_repo配下)
+urlpatterns = [
+    # 指定なし(day_repo/)
+    path("", day_repo_list_view),
+]
--- day_repo/views.py
+++ day_repo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# テンプレート用HTML表示メソッド
+def day_repo_list_view(request):
+    # テンプレート用HTML"day-repo-list.html"を描画
+    return render(request, "day_repo/day-repo-list.html")
