e1e6ed864903076ae6ce54d253f3aacde4bab501
Ginta-Togawa <togawa.ginta.ag@gmail.com>
2023-01-23 02:43:35 +0900

    レスポンス画面にcontext設定

--- day_repo/urls.py
+++ day_repo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from day_repo.views import day_repo_list_view
+from day_repo.views import day_repo_list_view, day_repo_detail_view
 
 # URLパターン(day_repo配下)
 urlpatterns = [
     # 指定なし(day_repo/)
     path("", day_repo_list_view),
+    # 詳細表示(day_repo/detail/)
+    path("detail/", day_repo_detail_view),
 ]
--- day_repo/views.py
+++ day_repo/views.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.shortcuts import render
 
 
@@ -7,3 +9,13 @@ from django.shortcuts import render
 def day_repo_list_view(request):
     # テンプレート用HTML"day-repo-list.html"を描画
     return render(request, "day_repo/day-repo-list.html")
+
+
+# 日報詳細表示
+def day_repo_detail_view(request):
+    template_name = "day_repo/day-repo-detail.html"
+    random_int = randint(1, 10)
+    context = {
+        "random_number": random_int,
+    }
+    return render(request, template_name, context)
