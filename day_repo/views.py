c2407e12312d867c2aed698f05a6ed24774d0990
Ginta-Togawa <togawa.ginta.ag@gmail.com>
2023-02-13 07:54:38 +0900

    LoginRequiredMixinで各処理をログイン済み出ないと使用できないように修正

--- day_repo/views.py
+++ day_repo/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
@@ -11,7 +12,7 @@ from day_repo.models import ReportModel
 # 1.modelで指定したデータベーステーブルからQuerySetを取得する
 # 2.「object_list」という変数にQuerySetを格納する
 # 3.HTMLテンプレートへコンテキストとしてQuerySetを渡す
-class ReportModeListView(ListView):  # クラス作成
+class ReportModeListView(LoginRequiredMixin, ListView):
     template_name = "day_repo/day-repo-list.html"
     model = ReportModel
 
@@ -20,13 +21,13 @@ class ReportModeListView(ListView):  # クラス作成
 # 1.ブラウザから「pk」を受け取る
 # 2.データベースから「pk」が一致するデータを取り出す
 # 3.コンテキストとしてHTMLテンプレートへ渡す
-class ReportModelDetailView(DetailView):
+class ReportModelDetailView(LoginRequiredMixin, DetailView):
     template_name = "day_repo/day-repo-detail.html"
     model = ReportModel
 
 
 # 日報作成入力・登録
-class ReportModelFormCreateView(CreateView):
+class ReportModelFormCreateView(LoginRequiredMixin, CreateView):
     template_name = "day_repo/day-repo-form.html"
     form_class = ReportModelForm
     success_url = reverse_lazy('day_repo_list')
@@ -39,7 +40,7 @@ class ReportModelFormCreateView(CreateView):
 
 
 # 日報編集入力・更新
-class ReportModelFormUpdateView(UpdateView):
+class ReportModelFormUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "day_repo/day-repo-form.html"
     model = ReportModel
     form_class = ReportModelForm
@@ -47,14 +48,14 @@ class ReportModelFormUpdateView(UpdateView):
 
 
 # 日報削除
-class ReportModelFormDeleteView(DeleteView):
+class ReportModelFormDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "day_repo/day-repo-delete.html"
     model = ReportModel
     success_url = reverse_lazy('day_repo_list')
 
 
 # 画像アップロード(ディレクトリ)
-class ImageUploadView(CreateView):
+class ImageUploadView(LoginRequiredMixin, CreateView):
     form_class = ImageUploadForm
     template_name = "day_repo/image-upload.html"
     success_url = "/day_repo"
