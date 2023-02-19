7b60363301c7e9d09c4e58ef77ea723ddc36c033
Ginta-Togawa <togawa.ginta.ag@gmail.com>
2023-02-19 16:51:42 +0900

    バリデーションチェックの実装

--- accounts/forms.py
+++ accounts/forms.py
@@ -67,3 +67,15 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ["user"]
+
+    # バリデーションチェック(ユーザ名)
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        user_email = self.instance.user.email
+        # ユーザ名に自身のメールアドレスが指定されたとき
+        if username == user_email:
+            raise forms.ValidationError("ユーザー名を変更してください")
+        # ユーザ名に@が有力されているとき
+        elif "@" in username:
+            raise forms.ValidationError("ユーザー名にEメールアドレスは使用できません")
+        return username
