376c0b0b3331ef90645de6edf31cf53b31cbdfea
Ginta-Togawa <togawa.ginta.ag@gmail.com>
2023-02-27 00:48:14 +0900

    アプリ用スキーマに変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,9 +102,9 @@ DATABASES = {
         'PASSWORD': 'django',
         'HOST': 'localhost',
         'PORT': '5432',
-        # 'OPTIONS': {
-        #     'options': '-c search_path=day_repo'
-        # },
+        'OPTIONS': {
+            'options': '-c search_path=day_repo'
+        },
         'TEST': {
             'NAME': 'test_db',
         },
